a2456e43241ddb694ac36caa18948990573341de
Walid Bouhedda <walidforfour@gmail.com>
2024-03-01 19:00:15 +0100

    Index and play pages

--- src/app.js
+++ src/app.js
@@ -1,1 +1,10 @@
-console.log("Hello, PathGuessr!");
+document.addEventListener("DOMContentLoaded", function () {
+    // Initialize the map
+    var map = L.map('map-container').setView([0, 0], 2);
+
+    // Add the image overlay
+    L.imageOverlay('images/gondwa.png', [[-90, -180], [90, 180]]).addTo(map);
+
+    // Set the bounds of the map
+    map.setMaxBounds([[-90, -180], [90, 180]]);
+});
